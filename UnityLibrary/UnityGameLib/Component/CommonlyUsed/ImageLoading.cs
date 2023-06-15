4db8b2a87ba79f3a9d087ab4a5bde0607b383998
shunsato <sdt22cge0013@edu.sca.ac.jp>
2023-06-15 16:30:13 +0900

    ライブラリの追加

---
+++ UnityLibrary/UnityGameLib/Component/CommonlyUsed/ImageLoading.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AddressableAssets;
+
+namespace CommonlyUsed
+{
+    public static class ImageLoading 
+    {
+        /// <summary>
+        /// 画像を指定のフォルダから読み込む関数
+        /// 引数(Image 返される画像, string 画像があるパス,bool 条件があれば)
+        /// </summary>
+        public static void ImageLoadingAsync(Image image, string imagePath)
+        {
+            Addressables.LoadAssetAsync<Sprite>(imagePath).Completed += sprite =>
+            {
+                image.sprite = Object.Instantiate(sprite.Result);
+            };
+        }
+    }
+}
